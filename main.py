d35df2c0ba61bd53f07708fd3786e0ef3f50b538
이성령 <leesungreong@gmail.com>
2023-04-23 16:46:38 +0900

    add time filter and message remain meeting room

--- main.py
+++ main.py
@@ -12,12 +12,23 @@ from PyQt5.QtWidgets import (
     QHBoxLayout,
     QVBoxLayout,
     QMessageBox,
+    QGroupBox,
 )
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import QDate, Qt
 from src.register_calender_cls import BizBoxCommon
 import configparser
 import os 
+import datetime
+
+def validate_time_format(time_str):
+    try:
+        datetime.datetime.strptime(time_str, '%H:%M')
+        return True
+    except ValueError:
+        return False
+
+
 def read_config():
     config = configparser.ConfigParser(interpolation=None)
     script_path = os.path.dirname(os.path.abspath(__file__))
@@ -93,6 +104,7 @@ class Schedule(QWidget):
         reg_label = QLabel("등록")
         reg_hbox.addWidget(reg_label)
         self.room_combo = QComboBox(self)
+        self.meeting_room_list = ["2F) 자율좌석 회의실", "2F) 대회의실", "2F) 소회의실", "3F) 대회의실", "3F) 소회의실", "3F) 중회의실"]
         self.room_combo.addItems(["2F) 자율좌석 회의실", "2F) 대회의실", "2F) 소회의실", "3F) 대회의실", "3F) 소회의실", "3F) 중회의실","Zoom1","Zoom2"])
         self.room_combo.setGeometry(300, 220, 100, 30)
         reg_hbox.addWidget(self.room_combo)
@@ -110,6 +122,21 @@ class Schedule(QWidget):
         btn_hbox.addWidget(self.filter_combo)
         btn_hbox.addWidget(self.view_btn)
 
+        
+        self.time_widget = QWidget()
+        # QHBoxLayout로 QPushButton을 가로로 배치
+        self.view_btn.clicked.connect(self.open_time_hbox)
+        self.time_hbox = QHBoxLayout()
+        time_label = QLabel("시간대 조회")
+        self.time_hbox.addWidget(time_label)
+        # 2. 버튼이 2개 있어야 함 (조회와 등록)
+        self.view_time_btn = QPushButton("특정 시간대에 남은 회의실 조회", self)
+        self.view_time_btn.setGeometry(300, 20, 100, 30)
+        self.view_time_btn.clicked.connect(self.view_time_schedule)
+        self.time_hbox.addWidget(self.view_time_btn)
+        self.time_widget.setLayout(self.time_hbox)
+        self.time_widget.setVisible(False)
+
         # QVBoxLayout로 QHBoxLayout을 세로로 배치
         vbox = QVBoxLayout()
         vbox.addLayout(date_hbox)
@@ -118,6 +145,7 @@ class Schedule(QWidget):
         vbox.addLayout(content_hbox)
         vbox.addLayout(attendees_hbox)
         vbox.addLayout(btn_hbox)
+        vbox.addWidget(self.time_widget)
         vbox.addLayout(reg_hbox)
 
         # 6. 회의실을 등록하면, 테이블에 추가 되는 기능
@@ -144,6 +172,9 @@ class Schedule(QWidget):
         self.bizbox = BizBoxCommon(user=self.config["USER"]["id"],pw=self.config["USER"]["pw"])
         self.open_bizbox()
 
+    def open_time_hbox(self):
+        self.time_widget.setVisible(True)
+
     def open_bizbox(self):
         self.bizbox.connect_site()
         self.bizbox.login()
@@ -163,6 +194,51 @@ class Schedule(QWidget):
         # 3. 캘린더를 선택하면 날짜가 보이는 텍스트 박스가 필요
         self.date_text.setText(date.toString(Qt.ISODate))
 
+    def check_time_format(self) :
+        start_time = self.start_time_text.text()
+        end_time = self.end_time_text.text()
+        if validate_time_format(start_time) :
+            pass 
+        else :
+            return QMessageBox.information(self,"알림", "시작 시간 형식이 잘못되었습니다.")
+        if validate_time_format(end_time) :
+            pass 
+        else :
+            return QMessageBox.information(self,"알림", "종료 시간 형식이 잘못되었습니다.")
+
+    def view_time_schedule(self) :
+        self.check_time_format()
+        date = self.date_text.text()
+        start_time = self.start_time_text.text()
+        end_time = self.end_time_text.text()
+        
+        if start_time > end_time :
+            return QMessageBox.information(self,"알림", "시작 시간이 종료 시간보다 늦습니다.")
+        else :
+            pass 
+
+        if self.table.rowCount() == 0 :
+            return QMessageBox.information(self,f"알림", "조회할 회의실이 하나도 존재 하지 않습니다. {start_time}~{end_time}")
+        else :
+            pass 
+        CHECK_COUNT = 0
+        used_meeeting_room = []
+        for row in range(self.table.rowCount()):
+            startTime = self.table.item(row, 1).text()  # 시작 시간
+            endTime = self.table.item(row, 2).text()  # 종료 시간
+            if startTime <= end_time and endTime >= start_time and (startTime >= start_time and startTime <= end_time) or (endTime >= start_time and endTime <= end_time) :
+                self.table.setRowHidden(row, False)  # 필터링 조건에 부합하는 경우 행 표시
+                CHECK_COUNT += 1 
+                used_meeeting_room.append(self.table.item(row, 3).text())
+            else:
+                self.table.setRowHidden(row, True)  # 필터링 조건에 부합하지 않는 경우 행 숨김
+        else :
+            remain_meeting_room_list = list(set(self.meeting_room_list).difference(set(used_meeeting_room)))
+            if len(remain_meeting_room_list) == 0 :
+                return QMessageBox.information(self,f"알림", f"{start_time}~{end_time} 시간대에 남은 회의실은 없습니다. 다른 시간대로 예약해주세요.")
+            else :
+                return QMessageBox.information(self,f"알림", f"{start_time}~{end_time} 시간대에 남은 회의실은 {', '.join(remain_meeting_room_list)} 입니다.")
+    
     def view_schedule(self):
         # 조회 버튼 클릭 시 실행되는 함수
         # 스케줄을 조회하는 기능을 추가해주세요.
@@ -184,13 +260,15 @@ class Schedule(QWidget):
                 self.table.setItem(row_count, 3, QTableWidgetItem(row["장소"]))
                 self.table.setItem(row_count, 4, QTableWidgetItem(row["회의명"]))
             else :
-                QMessageBox.information(self, "알림", f"날짜 : {date} 회의 스케줄이 조회되었습니다.")
+                QMessageBox.information(self, "알림", f"날짜 : {date}회의 스케줄이 조회되었습니다.")
+                
     def reg_schedule(self):
         # 등록 버튼 클릭 시 실행되는 함수
         # 스케줄을 등록하는 기능을 추가해주세요.
         date = self.date_text.text()
         start_time = self.start_time_text.text()
         end_time = self.end_time_text.text()
+        self.check_time_format()
         room = self.room_combo.currentText()
         content = self.content_text.text()
         if len(self.attendees_text.text()) == 0 :
@@ -223,3 +301,4 @@ if __name__ == "__main__":
     schedule.setWindowTitle("BizBox 회의 스케줄 관리 프로그램")
     schedule.show()
     sys.exit(app.exec_())
+
